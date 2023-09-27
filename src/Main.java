83fba55aa5981f10966460c0d3a0681626e5f142
Chaitanya Padalkar <125194952+chaitanyapadalkar@users.noreply.github.com>
2023-09-27 08:00:11 +0000

    Done :)

--- src/Main.java
+++ src/Main.java
@@ -16,32 +16,33 @@ public class Main{
         Scanner sc = new Scanner(System.in);
 
         while(!gameOver){
-            printBoard();
+            System.out.println();
+            printBoard(board);
+            System.out.println();
             
-            System.out.println("Player " + player + " enter: ");
+            System.out.print("Player " + player + " enter: ");
 
             int row = sc.nextInt();
             int col = sc.nextInt();
 
-            if(board[row][col] == ' '){
+            if(row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' '){
                 board[row][col] = player;
                 gameOver = haveWon(player,board);
 
                 if(gameOver){
-                    System.out.println("Player " + player + "has won!");
+                    System.out.println("Player " + player + " has won!");
                 }
                 else{
-                    player = player == 'X' ? 'X' : 'Y';
+                    player = (player == 'X') ? 'O' : 'X';
                 }
             }
             else{
                 System.out.println("Invalid move. Try Again");
             }
-
-            printBoard();
         }
+    }
 
-        public static void printBoard(){
+    public static void printBoard(char[][] board){
             for (int row = 0; row < board.length; row++) {
                 for (int col = 0; col < board[row].length; col++) {
                     System.out.print(board[row][col] + "|");
@@ -52,8 +53,25 @@ public class Main{
 
         public static boolean haveWon(char player, char[][] board){
             for (int i = 0; i < board.length; i++) {
-                if(board[i][col]==)
+                if(board[i][0]==player && board[i][1]==player && board[i][2]==player){
+                    return true;
+                }
             }
+
+            for (int i = 0; i < board.length; i++) {
+                if(board[0][i]==player && board[1][i]==player && board[2][i]==player){
+                    return true;
+                }
+            }
+
+            if(board[0][0]==player && board[1][1]==player && board[2][2]==player){
+                return true;
+            }
+
+            if(board[0][2]==player && board[1][1]==player && board[2][0]==player){
+                return true;
+            }            
+
+            return false;
         }
-    }
 }
